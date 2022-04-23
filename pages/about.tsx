0d9818e85ae48932485cfbf7d120abb69ff3b2c1
namnguyen <namnguyenba0602@gmail.com>
2022-04-23 22:59:08 +0700

    fetching

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export interface AboutProps {}
 
 export default function About(props: AboutProps) {
+	const router = useRouter()
+
+	useEffect(() => {
+		// check query available
+	}, [])
+
 	return <div>About Page</div>
 }
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,40 @@
 import * as React from 'react'
+import { GetStaticProps, GetStaticPropsContext } from 'next'
 
-export interface PostListProps {}
+export interface PostListProps {
+	posts: Post[]
+}
 
-export default function PostList(props: PostListProps) {
-	return <div>Post list page</div>
+interface Post {
+	id: number
+	title: string
+}
+
+export default function PostList({ posts }: PostListProps) {
+	return (
+		<>
+			<div>Post list page</div>
+			<ul>
+				{posts.map((post) => (
+					<li key={post.id}>{post.title}</li>
+				))}
+			</ul>
+		</>
+	)
+}
+
+export const getStaticProps: GetStaticProps<PostListProps> = async (
+	context: GetStaticPropsContext
+) => {
+	// server side
+	// build-time
+	console.log('static props')
+	const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+	const posts = await res.json()
+
+	return {
+		props: {
+			posts: posts.map((post: Post) => ({ id: post.id, title: post.title })),
+		},
+	}
 }
